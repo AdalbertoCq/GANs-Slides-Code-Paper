352d19151e2b2c959085a0f5ed119d05cadee4cd
Adalberto Claudio Quiros <aclaudioquiros@gmail.com>
2019-05-11 20:01:23 +0100

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,13 +1,46 @@
 import tensorflow as tf
+import math
 from models.generative.ops import *
 from models.generative.activations import *
 from models.generative.normalization import *
 
+
 display = True
 
-def generator_resnet(z_input, image_channels, layers, spectral, activation, reuse, is_train, normalization, attention=None, up='upscale'):
+
+def mapping_network(z_input, z_dim, layers, spectral, activation, normalization, init='xavier', regularizer=None):
+	if display:
+		print('MAPPING NETWORK INFORMATION:')
+		print('Layers:      ', layers)
+		print('Normalization: ', normalization)
+		print('Activation:    ', activation)
+		print()
+
+	with tf.variable_scope('mapping_network'):
+		net = z_input
+		for layer in range(layers):
+			net = dense(inputs=net, out_dim=z_dim, spectral=spectral, init=init, regularizer=regularizer, scope=layer)	
+		w_input = net
+
+	return w_input
+
+
+def generator_resnet(z_input, image_channels, layers, spectral, activation, reuse, is_train, normalization, init='xavier', regularizer=None, cond_label=None, attention=None, up='upscale', bigGAN=False):
 	channels = [32, 64, 128, 256, 512, 1024]
 	reversed_channel = list(reversed(channels[:layers]))
+
+	# Question here: combine z dims for upscale and the conv after, or make them independent.
+	if bigGAN:
+		z_dim = z_input.shape.as_list()[-1]
+		blocks = 2 + layers
+		block_dims = math.floor(z_dim/blocks)
+		remainder = z_dim - block_dims*blocks
+		if remainder == 0:
+			z_sets = [block_dims]*(blocks + 1)
+		else:
+			z_sets = [block_dims]*blocks + [remainder]
+		z_splits = tf.split(z_input, num_or_size_splits=z_sets, axis=-1)
+
 
 	if display:
 		print('GENERATOR INFORMATION:')
@@ -18,52 +51,130 @@ def generator_resnet(z_input, image_channels, layers, spectral, activation, reus
 		print()
 
 	with tf.variable_scope('generator', reuse=reuse):
+		if bigGAN: 
+			z_input_block = z_splits[0]
+			label = z_splits[1]
+		else:
+			z_input_block = z_input
+			label = z_input
+		if cond_label is not None: label = tf.concat([cond_label, label], axis=-1)
+
+		# Dense.			
+		net = dense(inputs=z_input_block, out_dim=1024, spectral=spectral, init=init, regularizer=regularizer, scope=1)			
+		# net = batch_norm(inputs=net, training=is_train)
+		# Introducing instance norm into dense layers.
+		net = normalization(inputs=net, training=is_train, c=label, spectral=spectral, scope='dense_1')
+		net = activation(net)
+
+		if bigGAN: label = z_splits[2]
+		else: label = z_input
+		if cond_label is not None: label = tf.concat([cond_label, label], axis=-1)
+
+		# Dense.
+		net = dense(inputs=net, out_dim=256*7*7, spectral=spectral, init=init, regularizer=regularizer, scope=2)				
+		# net = batch_norm(inputs=net, training=is_train)
+		# Introducing instance norm into dense layers.
+		net = normalization(inputs=net, training=is_train, c=label, spectral=spectral, scope='dense_2')
+		net = activation(net)
+		
+		# Reshape
+		net = tf.reshape(tensor=net, shape=(-1, 7, 7, 256), name='reshape')
+
+		for layer in range(layers):
+
+			if bigGAN: label = z_splits[3+layer] 
+			else: label = z_input
+			if cond_label is not None: label = tf.concat([cond_label, label], axis=-1)
+
+			# ResBlock.
+			net = residual_block(inputs=net, filter_size=3, stride=1, padding='SAME', scope=layer, is_training=is_train, spectral=spectral, init=init, regularizer=regularizer, 
+								 activation=activation, normalization=normalization, cond_label=label)
+			
+			# Attention layer. 
+			if attention is not None and net.shape.as_list()[1]==attention:
+				net = attention_block(net, spectral=True, init=init, regularizer=regularizer, scope=layers)
+			
+			# Up.
+			net = convolutional(inputs=net, output_channels=reversed_channel[layer], filter_size=2, stride=2, padding='SAME', conv_type=up, spectral=spectral, init=init, regularizer=regularizer, scope=layer)
+			net = normalization(inputs=net, training=is_train, c=label, spectral=spectral, scope=layer)
+			net = activation(net)
+			
+		logits = convolutional(inputs=net, output_channels=image_channels, filter_size=3, stride=1, padding='SAME', conv_type='convolutional', spectral=spectral, init=init, regularizer=regularizer, scope='logits')
+		output = sigmoid(logits)
+		
+	print()
+	return output
+
+
+def generator_decoder_resnet(z_input, image_channels, layers, spectral, activation, reuse, is_train, normalization, attention=None, up='upscale'):
+	channels = [32, 64, 128, 256, 512, 1024]
+	reversed_channel = list(reversed(channels[:layers]))
+
+	if display:
+		print('GENERATOR-DECODER INFORMATION:')
+		print('Channels:      ', channels[:layers])
+		print('Normalization: ', normalization)
+		print('Activation:    ', activation)
+		print('Attention H/W: ', attention)
+		print()
+
+	with tf.variable_scope('generator_decoder', reuse=reuse):
 		# Doesn't work ReLU, tried.
 
 		# Dense.			
 		net = dense(inputs=z_input, out_dim=1024, spectral=spectral, scope=1)				
 		net = normalization(inputs=net, training=is_train)
 		net = activation(net)
-		if display: 
-			print('Dense Layer:    dim.    1024 Output Shape: %s' % net.shape)
-
+		
 		# Dense.
 		net = dense(inputs=net, out_dim=256*7*7, spectral=spectral, scope=2)				
 		net = normalization(inputs=net, training=is_train)
 		net = activation(net)
-		if display: 
-			print('Dense Layer:    dim. 256*7*7 Output Shape: %s' % net.shape)
-
+		
 		# Reshape
 		net = tf.reshape(tensor=net, shape=(-1, 7, 7, 256), name='reshape')
 
 		for layer in range(layers):
+
 			# ResBlock.
 			net = residual_block(inputs=net, filter_size=3, stride=1, padding='SAME', scope=layer, is_training=is_train, spectral=spectral, activation=activation, normalization=normalization)
-			if display:
-				print('ResBlock Layer: channels %4s filter_size=3, stride=1, padding=SAME, conv_type=convolutional scope=%s Output Shape: %s' % (reversed_channel[layer], layer, net.shape))
-
+		
 			# Attention layer. 
 			if attention is not None and net.shape.as_list()[1]==attention:
 				net = attention_block(net, spectral=True, scope=layers)
-				print('Att. Layer    : channels %4s' % reversed_channel[layer])
+		
+			# if (vae_dim/2.) == net.shape.as_list()[1]:
+			# 	lr_logs2_xi_z = convolutional(inputs=net, output_channels=reversed_channel[layer], filter_size=2, stride=2, padding='SAME', conv_type=up, spectral=spectral, scope='lr_logs2_xi_z')
+		
+			# if (vae_dim/2.) == net.shape.as_list()[1]:
+			# 	scope = 'lr_mean_xi_z'
+			# else:
+			# 	scope = layer
 
 			# Up.
 			net = convolutional(inputs=net, output_channels=reversed_channel[layer], filter_size=2, stride=2, padding='SAME', conv_type=up, spectral=spectral, scope=layer)
 			net = normalization(inputs=net, training=is_train)
 			net = activation(net)
-			if display:
-				print('Conv Layer:     channels %4s filter_size=2,  stride=2, padding=SAME, conv_type=%s scope=%s Output Shape: %s' % (reversed_channel[layer], up, layer, net.shape))
 
-		logits = convolutional(inputs=net, output_channels=image_channels, filter_size=3, stride=1, padding='SAME', conv_type='convolutional', spectral=spectral, scope=layer+1)
-		output = sigmoid(logits)
-		if display: 
-			print('Logits Layer:   channels %4s filter_size=3, stride=1, padding=SAME, conv_type=convolutional scope=%s Output Shape: %s' % (image_channels, layer+1, net.shape))
+			# if vae_dim == net.shape.as_list()[1]:
+			# 	lr_mean_xi_z = sigmoid(net)
 
+			
+		# Final outputs
+		logits = convolutional(inputs=net, output_channels=image_channels, filter_size=3, stride=1, padding='SAME', conv_type='convolutional', spectral=spectral, scope='mean_xi_z')
+		mean_xi_z = sigmoid(logits)
+
+		# Final outputs
+		logs2_xi_z = convolutional(inputs=net, output_channels=image_channels, filter_size=3, stride=1, padding='SAME', conv_type='convolutional', spectral=spectral, scope='logs2_xi_z')
+
+		
 	print()
-	return output
+	# return output, lr_mean_xi_z, lr_logs2_xi_z
+	return mean_xi_z, logs2_xi_z
+
 
 def generator_resnet_cond(z_input, c_input, image_channels, layers, spectral, activation, reuse, is_train, normalization, up='upscale'):
+	channels = [32, 64, 128, 256, 512, 1024]
 	channels = [32, 64, 128, 256, 512, 1024]
 	reversed_channel = list(reversed(channels[:layers]))
 
@@ -83,16 +194,12 @@ def generator_resnet_cond(z_input, c_input, image_channels, layers, spectral, ac
 		net = dense(inputs=net, out_dim=1024, spectral=spectral, scope=1)				
 		net = normalization(inputs=net, training=is_train)
 		net = activation(net)
-		if display: 
-			print('Dense Layer:    dim.    1024 Output Shape: %s' % net.shape)
-
+		
 		# Dense.
 		net = dense(inputs=net, out_dim=256*7*7, spectral=spectral, scope=2)				
 		net = normalization(inputs=net, training=is_train)
 		net = activation(net)
-		if display: 
-			print('Dense Layer:    dim. 256*7*7 Output Shape: %s' % net.shape)
-
+		
 		# Reshape
 		net = tf.reshape(tensor=net, shape=(-1, 7, 7, 256), name='reshape')
 
@@ -100,21 +207,15 @@ def generator_resnet_cond(z_input, c_input, image_channels, layers, spectral, ac
 			# ResBlock.
 			net = residual_block(inputs=net, filter_size=3, stride=1, padding='SAME', scope=layer, is_training=is_train, spectral=spectral,
 								 activation=activation, normalization=normalization, c_input=c_input)
-			if display:
-				print('ResBlock Layer: channels %4s filter_size=3, stride=1, padding=SAME, conv_type=convolutional scope=%s Output Shape: %s' % (reversed_channel[layer], layer, net.shape))
-
+		
 			# Up.
 			net = convolutional(inputs=net, output_channels=reversed_channel[layer], filter_size=2, stride=2, padding='SAME', conv_type=up, spectral=spectral, scope=layer)
 			net = normalization(inputs=net, training=is_train, c=c_input, spectral=spectral)
 			net = activation(net)
-			if display:
-				print('Conv Layer:     channels %4s ilter_size=2,  stride=2, padding=SAME, conv_type=%s scope=%s Output Shape: %s' % (reversed_channel[layer], up, layer, net.shape))
-
-		logits = convolutional(inputs=net, output_channels=image_channels, filter_size=3, stride=1, padding='SAME', conv_type='convolutional', spectral=spectral, scope=layer+1)
+		
+		logits = convolutional(inputs=net, output_channels=image_channels, filter_size=3, stride=1, padding='SAME', conv_type='convolutional', spectral=spectral, scope='logits')
 		output = sigmoid(logits)
-		if display: 
-			print('Logits Layer:   channels %4s filter_size=3, stride=1, padding=SAME, conv_type=convolutional scope=%s Output Shape: %s' % (image_channels, layer+1, net.shape))
-
+		
 	print()
 	return output
 
@@ -132,13 +233,11 @@ def generator(z_input, image_channels, layers, spectral, activation, reuse, is_t
 		# Doesn't work ReLU, tried.
 		
 		# Dense.
-		if display: print('Dense Layer: Dim=1024')
 		net = dense(inputs=z_input, out_dim=1024, spectral=spectral, scope=1)				
 		net = normalization(inputs=net, training=is_train)
 		net = activation(net)
 
 		# Dense.
-		if display: print('Dense Layer: Dim=256*7*7')
 		net = dense(inputs=net, out_dim=256*7*7, spectral=spectral, scope=2)				
 		net = normalization(inputs=net, training=is_train)
 		net = activation(net)
@@ -148,21 +247,18 @@ def generator(z_input, image_channels, layers, spectral, activation, reuse, is_t
 
 		for layer in range(layers):
 			# Conv.
-			if display: print('Conv Layer: channels %s filter_size=2, stride=2, padding=SAME, conv_type=transpose scope=%s' % (reversed_channel[layer], 2*(layer+1)-1))
 			net = convolutional(inputs=net, output_channels=reversed_channel[layer], filter_size=2, stride=2, padding='SAME', conv_type='transpose', spectral=spectral, scope=2*(layer+1)-1)
 			net = normalization(inputs=net, training=is_train)
 			net = activation(net)
 
 			if layer != len(range(layers))-1:
 				# Conv.
-				if display: print('Conv Layer: channels %s filter_size=5, stride=1, padding=SAME, conv_type=convolutional scope=%s' % (reversed_channel[layer+1], 2*(layer+1)))
 				net = convolutional(inputs=net, output_channels=reversed_channel[layer+1], filter_size=5, stride=1, padding='SAME', conv_type='convolutional', spectral=spectral, scope=2*(layer+1))
 				net = normalization(inputs=net, training=is_train)
 				net = activation(net)
 
 		# Conv.
-		if display: print('Logits Layer: channels %s filter_size=2, stride=2, padding=SAME, conv_type=convolutional scope=%s' % (image_channels, 2*(layer+1)))
-		logits = convolutional(inputs=net, output_channels=image_channels, filter_size=2, stride=2, padding='SAME', conv_type='transpose', spectral=spectral, scope=2*(layer+1))
+		logits = convolutional(inputs=net, output_channels=image_channels, filter_size=2, stride=2, padding='SAME', conv_type='transpose', spectral=spectral, scope='logits')
 		output = sigmoid(logits)
 	
 	print()
